b5001e9620a643078e59626d57f2c72796a2c1cf
Nitin Mali <malinitin.jd@gmail.com>
2026-06-05 07:48:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (19.28%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minimumCost(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        cost.sort(reverse=True)
+        total = 0
+
+        for i in range(len(cost)):
+            if i % 3 != 2:
+                total += cost[i]
+
+        return total
