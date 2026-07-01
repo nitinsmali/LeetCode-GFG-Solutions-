72ea5ba2ce7e590a1317706c2190e25e40dcbc2a
Nitin Mali <malinitin.jd@gmail.com>
2026-07-01 17:33:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getSecondLargest(self, arr):
+        # code here
+        largest = -1
+        second_largest = -1
+        
+        for num in arr:
+            if num > largest:
+                second_largest = largest
+                largest = num
+                
+            elif num > second_largest and num < largest:
+                second_largest = num
+                
+        return second_largest
+                
