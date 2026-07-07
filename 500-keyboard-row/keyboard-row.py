b1f500805399c84b2b906877152407c80d52610f
Nitin Mali <malinitin.jd@gmail.com>
2026-07-07 14:58:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.13%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+        
+        answer = []
+        for word in words:
+            lower_word = set(word.lower())
+            if lower_word.issubset(row1) or lower_word.issubset(row2) or lower_word.issubset(row3):
+                answer.append(word)
+                
+        return answer
