2b79ce506dbdff6372b185122b7d98d33c281069
Nitin Mali <malinitin.jd@gmail.com>
2026-07-30 15:13:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (100.00%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minimumPushes(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        n = len(word)
+        ans = 0
+        
+        for i in range(n):
+            ans += (i // 8) + 1
+            
+        return ans
+        
