da862cb315a6071ea701f4aa899766bd9f8726d2
Nitin Mali <malinitin.jd@gmail.com>
2026-08-01 09:02:05 +0530

    Time: 4 ms (39.31%) | Memory: 12.4 MB (73.41%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def predictTheWinner(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        if n % 2 == 0:
+            return True
+            
+        dp = list(nums)
+        
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i] = max(nums[i] - dp[i + 1], nums[j] - dp[i])
+                
+        return dp[0] >= 0
+            
